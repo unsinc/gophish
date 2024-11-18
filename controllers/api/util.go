6f5e306ca1379edcb453f309f1327aec51f313c0
unsinc <156654990+unsinc@users.noreply.github.com>
2024-11-18 18:48:37 +0200

    message changes

--- controllers/api/util.go
+++ controllers/api/util.go
@@ -34,14 +34,10 @@ func (as *Server) SendTestEmail(w http.ResponseWriter, r *http.Request) {
 	// If a Template is not specified use a default
 	if s.Template.Name == "" {
 		//default message body
-		text := "It works!\n\nThis is an email letting you know that your gophish\nconfiguration was successful.\n" +
-			"Here are the details:\n\nWho you sent from: {{.From}}\n\nWho you sent to: \n" +
-			"{{if .FirstName}} First Name: {{.FirstName}}\n{{end}}" +
-			"{{if .LastName}} Last Name: {{.LastName}}\n{{end}}" +
-			"{{if .Position}} Position: {{.Position}}\n{{end}}" +
-			"\nNow go send some phish!"
+		text := "It works\n" +
+			"\nReady Steady Go!"
 		t := models.Template{
-			Subject: "Default Email from Gophish",
+			Subject: "Default message",
 			Text:    text,
 		}
 		s.Template = t
